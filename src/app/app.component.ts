8f17e26b18b7d25813a0357a564e6719cb7c8d12
fsajadi <s.sfatemeh@gmail.com>
2020-01-28 11:13:07 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WrappedNodeExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  prepositionList = ["of", "the"]
   title = 'TitleCaseFormatter';
+  username;
+
+  splitter(username) {
+
+    let splitted = this.username.split(" ");
+    let myArray = []
+
+    for (let word of splitted) {
+      let lowerWord = word.toLowerCase();
+      if (this.prepositionList.includes(lowerWord)) {
+        myArray.push(lowerWord.slice(0).toLowerCase())
+      } else {
+        myArray.push(lowerWord.charAt(0).toUpperCase() + lowerWord.slice(1).toLowerCase())
+      }
+    }
+    if (this.prepositionList.includes(splitted[0])) {
+      myArray[0] = splitted[0].charAt(0).toUpperCase() + splitted[0].slice(1).toLowerCase()
+    }
+    console.log(myArray)
+  }
 }
